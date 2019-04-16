59c88abf58829b87ab9793bfbae8d48bc08c0a61
ljke <ljke1995@gmail.com>
2019-04-16 22:02:41 +0800

    add graph algorithm

---
+++ src/graph/AllPairShortestPath.java
@@ -0,0 +1,237 @@
+package graph;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.IntStream;
+
+/**
+ * 3种有向图的最短路径算法
+ */
+public class AllPairShortestPath {
+    //不要设置为Integer.MAX_VALUE，除非算法中已经排除了不可达结点，否则会溢出
+//    private static final int INF = 99999;
+    private static final int INF = Integer.MAX_VALUE;
+
+    /**
+     * Floyd算法，时间复杂度O(V^3)
+     */
+    public void floydWarshall(int[][] graph, int src, int dst, int v) {
+        int[] prev = new int[v];
+        Arrays.fill(prev, -1);
+
+        int i, j, k;
+
+        int[][] dist = new int[v][v];
+        //初始化一个距离数组
+        for (i = 0; i < v; i++) {
+            for (j = 0; j < v; j++) {
+                dist[i][j] = graph[i][j];
+            }
+        }
+
+        //插入k结点，更新距离矩阵
+        for (k = 0; k < v; k++) {
+            for (i = 0; i < v; i++) {
+                for (j = 0; j < v; j++) {
+                    //排除不可达结点
+                    if(dist[i][k] == INF || dist[k][j] == INF){
+                        continue;
+                    }
+                    if (dist[i][k] + dist[k][j] < dist[i][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                        prev[j] = k; //更新前驱结点
+                    }
+                }
+            }
+            printSolution(dist, v);
+        }
+        if (dist[src][dst] == INF) {
+            System.out.println("No found");
+        } else {
+            System.out.println("Shortest distance:" + dist[src][dst]);
+            printPath(src, dst, prev);
+        }
+    }
+
+    /**
+     * Dijkstra算法，时间复杂度O(V^2)
+     */
+    public void dijkstra(int[][] graph, int src, int dst, int v) {
+        //注意对数组进行初始化
+        boolean[] visited = new boolean[v];
+        visited[src] = true;
+        int[] prev = new int[v];
+        Arrays.fill(prev, -1);
+        for (int n : graph[src]) {
+            if (n < INF) {
+                prev[src] = src;
+            }
+        }
+        int[] costs = Arrays.copyOf(graph[src], v); //保存当前计算的最短路径
+
+        int cost;
+        int new_cost;
+        int node = findLowestCost(visited, costs, v);
+        while (node != dst && node != -1) { //当node == dst时，已经找到了到达dst的最短路径，可以结束
+            cost = costs[node];
+            int[] neighbor = graph[node];
+            for (int i = 0; i < v; i++) {
+                if (neighbor[i] == INF) { //排除不可达结点
+                    continue;
+                }
+                new_cost = cost + neighbor[i];
+                if (new_cost < costs[i]) {
+                    costs[i] = new_cost;
+                    prev[i] = node;
+                }
+            }
+            visited[node] = true;
+            node = findLowestCost(visited, costs, v);
+        }
+
+        if (costs[dst] == INF) {
+            System.out.println("No found");
+        } else {
+            System.out.println("Shortest distance:" + costs[dst]);
+            printPath(src, dst, prev);
+        }
+    }
+
+    /**
+     * Bellman-Ford算法，可以计算负权边，时间复杂度为O(V * E)
+     */
+    public void bellmanFord(int[][] graph, int src, int dst, int vertex) {
+        int[] dist = new int[vertex];
+        Arrays.fill(dist, INF);
+        dist[src] = 0;
+        int[] prev = new int[vertex];
+        Arrays.fill(prev, -1);
+        //算法主要考虑的是边，所以将邻接矩阵转换为三元组
+        List<int[]> edges = adjacencyToEdge(graph, vertex);
+
+        int u, v, w;
+        boolean updated; //改进：如果循环没有更新，提前跳出循环
+        for (int i = 0; i < vertex - 1; i++) {
+            updated = false;
+            for (int[] edge : edges) {
+                u = edge[0];
+                v = edge[1];
+                w = edge[2];
+                if (dist[u] != INF && (dist[u] + w) < dist[v]) {
+                    dist[v] = dist[u] + w;
+                    prev[v] = u;
+                    updated = true;
+                }
+            }
+            if(!updated){
+                break;
+            }
+        }
+
+        //检测是否存在负环路
+        for (int[] edge:edges) {
+            u = edge[0];
+            v = edge[1];
+            w = edge[2];
+            if (dist[u] != INF && (dist[u] + w) < dist[v]) {
+                dist[v] = dist[u] + w;
+                System.out.println("Negative loop existed");
+                return;
+            }
+        }
+
+        if (dist[dst] == INF) {
+            System.out.println("No found");
+        } else {
+            System.out.println("Shortest distance:" + dist[dst]);
+            printPath(src, dst, prev);
+        }
+    }
+
+    /**
+     * Dijkstra 算法中用于查找最小结点
+     *
+     * @param processed 已处理顶点数组，true表示已处理
+     * @param costs     从原点到每个顶点的cost
+     * @return 未处理的顶点中cost最小的顶点
+     */
+    private int findLowestCost(boolean[] processed, int[] costs, int v) {
+        //使用Java 8 stream API简化编程
+        //注意通常来说数组stream是不能够操作下标的，所以这里需要一些技巧
+        return IntStream.range(0, v).filter(i -> !processed[i]).boxed()
+                .min(Comparator.comparingInt(x -> costs[x])).orElse(-1);
+    }
+
+    /**
+     * Floyd算法中用于打印距离矩阵
+     *
+     * @param dist
+     * @param v
+     */
+    private void printSolution(int[][] dist, int v) {
+        System.out.println("Distance:");
+        for (int i = 0; i < v; i++) {
+            for (int j = 0; j < v; j++) {
+                if (dist[i][j] == INF) {
+                    System.out.print("INF\t");
+                } else {
+                    System.out.print(dist[i][j] + "\t");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    /**
+     * 在bellman算法中用于将邻接表转化为三元组(u, v, w)
+     * @param graph
+     * @param v
+     */
+    private List<int[]> adjacencyToEdge(int[][] graph, int v){
+        List<int[]> edge = new ArrayList<>();
+        for(int i = 0; i < v; i++){
+            for(int j = 0; j < v; j++){
+                if(graph[i][j] != 0 && graph[i][j] != INF){
+                    int[] tuple = {i, j, graph[i][j]};
+                    edge.add(tuple);
+                }
+            }
+        }
+        return edge;
+    }
+
+
+    /**
+     * 使用prev数组打印路径
+     *
+     * @param dst
+     * @param prev
+     */
+    private void printPath(int src, int dst, int[] prev) {
+        System.out.print("path:" + src);
+        recurPrint(src, dst, prev);
+        System.out.println();
+    }
+
+    private void recurPrint(int src, int dst, int[] prev) {
+        if (dst != -1 && dst != src) {
+            recurPrint(src, prev[dst], prev);
+        } else {
+            return;
+        }
+        System.out.print("->" + dst);
+    }
+
+    public static void main(String[] args) {
+        //使用邻接矩阵存储图
+        int[][] graph = {{0, 5, INF, 10}, {INF, 0, 3, INF}, {INF, INF, 0, 1}, {INF, INF, INF, 0}};
+        int v = graph.length;
+        AllPairShortestPath a = new AllPairShortestPath();
+        a.floydWarshall(graph, 0, 3, v);
+        a.dijkstra(graph, 0, 3, v);
+        a.bellmanFord(graph, 0, 3, v);
+    }
+
+}
--- src/graph/Graph.java
+++ src/graph/Graph.java
@@ -1,64 +1,91 @@
 package graph;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
+/**
+ * 图算法，BFS和DFS
+ */
 public class Graph {
-    private int v; //顶点个数
-    private LinkedList<Integer>[] adj;  // LinkedList array: 邻接表
-    //private boolean found = false; //dfs中表示找到路径
+    private int v;
+    private List<List<Integer>> adjacency; //邻接表存储
 
     public Graph(int v) {
         this.v = v;
-        adj = new LinkedList[v];
+        adjacency = new ArrayList<>(v);
         for (int i = 0; i < v; i++) {
-            adj[i] = new LinkedList<>();
+            List<Integer> arr = new ArrayList<>();
+            adjacency.add(arr);
         }
     }
 
-    public void addEdge(int s, int t) {
-        adj[s].add(t);
-        adj[t].add(s);
+    public void addEdge(int src, int dst) {
+        //无向图添加双向
+        adjacency.get(src).add(dst);
+        adjacency.get(dst).add(src);
     }
 
-    public void printPath(int s, int t, int[] prev) {
-        if (t != s) {
-            printPath(s, prev[t], prev);
+    //通过prev列表生成，注意是反向的
+    public String generatePath(int src, int dst, int[] prev) {
+        StringBuilder str = new StringBuilder();
+        while (prev[dst] != -1 && dst != src) {
+            str.append(dst).append("<-");
+            dst = prev[dst];
         }
-        System.out.print(" -> " + t);
+        return str.append(src).toString();
     }
 
-    public void bfs(int s, int t) {
-        if (s == t) {
+    //使用递归就能够正向打印
+    public void printPath(int src, int dst, int[] prev){
+        if(prev[dst] != -1 && dst != src){
+            printPath(src, prev[dst], prev);
+        }else{
             return;
         }
-        boolean[] visited = new boolean[v]; //已访问顶点
-        for (int i = 0; i < v; i++) {
-            visited[i] = false;
-        }
-        int[] prev = new int[v]; //前驱顶点
-        for (int i = 0; i < v; i++) {
-            prev[i] = -1;
-        }
-        Queue<Integer> queue = new LinkedList<>(); //使用队列保存待检查顶点
-        queue.add(s);
-        visited[s] = true; //只要被添加到queue中，就算是被访问过了
+        System.out.print("->" + dst);
+    }
 
-        while (queue.size() != 0) {
-            int w = queue.poll();
+
+    /**
+     * 广度优先搜索算法(BFS)，查找最短段数的路径
+     * BFS需要队列
+     * @param src
+     * @param dst
+     */
+    public void bfs(int src, int dst) {
+        if (src == dst) {
+            return;
+        }
+//        List<Boolean> visited = new ArrayList<>(Collections.nCopies(v, false));
+//        List<Integer> prev = new ArrayList<>(Collections.nCopies(v, -1));
+        //使用数组存储即可
+        boolean[] visited = new boolean[v];  //已访问顶点
+        int[] prev = new int[v]; //前驱顶点
+        Arrays.fill(visited, false);
+        Arrays.fill(prev, -1);
+
+        Queue<Integer> queue = new LinkedList<>(); //使用队列保存待检查顶点
+
+        visited[src] = true; //只要被添加到queue中，就算是被访问过了
+        queue.offer(src);
+        int v;
+        List<Integer> neighbour;
+        while (!queue.isEmpty()) {
+            v = queue.poll();
             //遍历w的所有邻接顶点
-            for (int i = 0; i < adj[w].size(); i++) {
-                int q = adj[w].get(i);
+            neighbour = adjacency.get(v);
+            for (int n : neighbour) {
                 //只处理未访问过的顶点
-                if (!visited[q]) {
-                    prev[q] = w; //记录前驱顶点
-                    if (q == t) {
-                        printPath(s, q, prev);
+                if (!visited[n]) {
+                    prev[n] = v; //记录前驱顶点
+                    if (n == dst) {
+                        System.out.println(generatePath(src, dst, prev));
+                        System.out.print(src);
+                        printPath(src, dst, prev);
                         System.out.println();
                         return;
                     } else {
-                        queue.add(q);
-                        visited[q] = true;
+                        queue.offer(n);
+                        visited[n] = true;
                     }
                 }
             }
@@ -66,58 +93,61 @@ public class Graph {
         System.out.println("No found");
     }
 
-    public void dfs(int s, int t) {
-        boolean[] visited = new boolean[v]; //已访问顶点
-        for (int i = 0; i < v; i++) {
-            visited[i] = false;
+    /**
+     * 深度优先搜索算法(DFS),找出一条可以到达的路径，不一定是最短的
+     * DFS借助于回溯思想，使用递归栈实现
+     * @param src
+     * @param dst
+     */
+    public void dfs(int src, int dst){
+        if (src == dst) {
+            return;
         }
-        int[] prev = new int[v]; //前驱顶点
-        for (int i = 0; i < v; i++) {
-            prev[i] = -1;
-        }
-        if (recurDFS(s, t, visited, prev)) {
-            printPath(s, t, prev);
-            System.out.println();
-        } else {
+        boolean[] visited = new boolean[v];
+        int[] prev = new int[v];
+        Arrays.fill(visited, false);
+        Arrays.fill(prev, -1);
+
+        boolean res = recurDFS(src, dst, visited, prev);
+        if (res) {
+            System.out.println(generatePath(src, dst, prev));
+        }else{
             System.out.println("No found");
         }
-
     }
 
-    public boolean recurDFS(int w, int t, boolean[] visited, int[] prev) {
-        //带有返回值的递归函数可以不使用found标志变量
-        //if (found) return true;
-        visited[w] = true; //遍历到当前顶点
-        if (w == t) { //当前顶点符合，直接返回
-            //found = true;
+    public boolean recurDFS(int src, int dst, boolean[] visited, int[] prev){
+        visited[src] = true; //遍历到当前顶点
+        if(src == dst){ //当前顶点符合，直接返回
             return true;
         }
         boolean ret = false;
-        for (int i = 0; i < adj[w].size(); i++) {
-            if (ret) return true;
-            int q = adj[w].get(i);
-            if (!visited[q]) { //只检查未访问过的顶点
-                prev[q] = w;
-                ret = recurDFS(q, t, visited, prev);
+        for (int neighbour :
+                adjacency.get(src)) {
+            if(ret){
+                return true;
+            }
+            if (!visited[neighbour]) { //只检查未访问过的顶点
+                prev[neighbour] = src;
+                ret = recurDFS(neighbour, dst, visited, prev);
             }
         }
         return ret;
     }
 
-    public static void main(String[] args) {
-        Graph graph = new Graph(8);
-        graph.addEdge(0, 1);
-        graph.addEdge(0, 3);
-        graph.addEdge(1, 2);
-        graph.addEdge(1, 4);
-        graph.addEdge(2, 5);
-        graph.addEdge(3, 4);
-        graph.addEdge(4, 5);
-        graph.addEdge(4, 6);
-        graph.addEdge(5, 7);
-        graph.addEdge(6, 7);
 
-        graph.bfs(0, 7);
-        graph.dfs(0, 7);
+    public static void main(String[] args) {
+        Graph g = new Graph(8);
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 5);
+        g.addEdge(1, 6);
+        g.addEdge(2, 3);
+        g.addEdge(5, 4);
+        g.addEdge(6, 7);
+        g.addEdge(3, 4);
+        g.addEdge(4, 7);
+        g.bfs(0, 7);
+        g.dfs(0, 7);
     }
 }
